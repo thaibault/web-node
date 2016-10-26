da55b0d3ead63e2929dcc475aca1f51c5403842d
torben <t.sickert[~at~]gmail.com>
2016-10-26 14:26:44 +0200

    Automatic update to new version.

--- configurator.js
+++ configurator.js
@@ -14,9 +14,7 @@
 */
 // region imports
 import Tools from 'clientnode'
-/* eslint-disable no-duplicate-imports */
 import type {PlainObject} from 'clientnode'
-/* eslint-enable no-duplicate-imports */
 import fileSystem from 'fs'
 import path from 'path'
 // NOTE: Only needed for debugging this file.
