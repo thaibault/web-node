034d5dc65eb4e6b6ee5742fead02149e3031fd5a
torben <t.sickert[~at~]gmail.com>
2016-11-09 19:05:40 +0100

    Automatic update to new version.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const main:ProcedureFunction = async ():Promise<any> => {
     const {configuration, plugins}:{
         configuration:Configuration;
         plugins:Array<Plugin>;
-    } = await PluginAPI.loadAll(Tools.copyRecursively(
+    } = await PluginAPI.loadAll(Tools.copyLimitedRecursively(
         baseConfiguration, -1, null, true))
     await PluginAPI.callStack('initialize', plugins, configuration)
     if (plugins.length)
