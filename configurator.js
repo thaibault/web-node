91c2ea4d034201dcbccf72928c800e556e1e855f
torben <t.sickert[~at~]gmail.com>
2017-03-09 18:01:47 +0100

    Automatic update to new version.

--- configurator.js
+++ configurator.js
@@ -82,7 +82,7 @@ if (name)
 packageConfiguration.webNode.name =
     packageConfiguration.documentationWebsite.name
 const parameterDescription:Array<string> = [
-    'currentPath', 'fileSystem', 'path', 'pluginAPI', 'require', 'tools',
+    'currentPath', 'fileSystem', 'path', 'PluginAPI', 'require', 'Tools',
     'webNodePath']
 let parameter:Array<any> = [
     /* eslint-disable no-eval */
